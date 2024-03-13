4990a34319f017e5c41def0cae1a336cdc63b4a5
ruslana zhold <ruslana.zhold@gmail.com>
2024-03-13 09:26:47 -0500

    jfjf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-2"  
+}
+resource "aws_vpc" "test" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test"
+  }
+}
+resource "aws_instance" "test" {
+  ami           = "ami-022661f8a4a1b91cf"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "ruslana-Instance"
+  }
+}

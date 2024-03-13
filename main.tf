4bb8eecd5146aa1abdbdbf51871d930a2c82f7ad
ruslana zhold <ruslana.zhold@gmail.com>
2024-03-13 11:22:00 -0500

    changed name of ec2

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ resource "aws_instance" "test" {
   ami           = "ami-022661f8a4a1b91cf"
   instance_type = "t2.micro"
   tags = {
-      Name = "ruslana-Instance"
+      Name = "webhook-Instance"
   }
 }
